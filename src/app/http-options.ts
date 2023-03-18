782a771ebacd703ad7cac4d2119a242b8bf07895
Alejandro Antelo <alfasant@protonmail.com>
2023-03-18 23:14:26 +0100

    refactor

--- src/app/http-options.ts
+++ src/app/http-options.ts
@@ -1,7 +1,15 @@
-import { HttpHeaders } from "@angular/common/http";
+import { HttpHeaders, HttpParams } from "@angular/common/http";
 
 export class HttpOptions {
   headers = new HttpHeaders({
     'Content-Type': 'application/json',
   })
+  params: HttpParams
+
+  constructor(params?: string) {
+    if (params)
+      this.params = new HttpParams({fromString: params})
+    else
+      this.params = new HttpParams()
+  }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -12,16 +12,16 @@ import { User } from './user';
 })
 export class UserService {
   private userUrl = 'https://localhost:8443/api/users'
-  private httpOptions = new HttpOptions()
   constructor(
     private http: HttpClient,
     private authService: AuthService
   ) { }
 
   getUserData(): Observable<User> {
-    // const url = 'https://localhost:8443/api/users/?token=627db4ee0a6dbb6177b5e6eb509e4f0906b5e1a98b7067f1cbd5e7544d1a763772bb43da28fdff3372e1c5520a2d2600bc779fe8cb50a6a36efd3e269c9fbc05c347fe2a75385dc0d14322e6c374c245c7caf0aa061c2166e1d05e07533f4fda'
-    const url = this.userUrl + '/?token='+this.authService.token
-    return this.http.get<User>(url, this.httpOptions)
+    const httpOptions = new HttpOptions('token='+this.authService.token)
+    console.log(httpOptions)
+    const url = this.userUrl + '/'
+    return this.http.get<User>(url, httpOptions)
       .pipe(catchError(this.handleError<User>()))
   }
 
