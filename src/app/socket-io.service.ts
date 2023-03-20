a08acccaa8fafff2d859ac544bcc9c3399b4a64b
Alejandro Antelo <alfasant@protonmail.com>
2023-03-20 23:18:53 +0100

    socketio works now

--- src/app/socket-io.service.ts
+++ src/app/socket-io.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { io } from 'socket.io-client'
+import { Message } from './message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketIoService {
-  private socket = io()
+  private socketUrl = 'https://localhost:8443/'
+  private socket = io(this.socketUrl)
   constructor(
   ) { }
 
@@ -15,5 +17,9 @@ export class SocketIoService {
     this.socket.on('refresh-messages', msg => {
       console.log(msg)
     })
+  }
+
+  sendMessage(message: string) {
+    this.socket.emit('message', message)
   }
 }
